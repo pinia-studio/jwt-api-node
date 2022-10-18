b1bb14a24b03859573f0fceb4158946b14689f37
Sulaymon-dev <40419449+Sulaymon-dev@users.noreply.github.com>
2022-10-18 15:37:58 +0500

    rc2

--- server.js
+++ server.js
@@ -1,31 +1,73 @@
-﻿require('rootpath')();
-const express = require('express');
+﻿const express = require("express");
 const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const jwt = require('./helpers/jwt');
-const errorHandler = require('./helpers/error-handler');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+const config = require("./config.json");
+const users = require("./users");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 app.use(cors());
 
-// use JWT auth to secure the api
-app.use(jwt());
+app.get("/profile", verifyJwt, (req, res) => {
+  const token = req.headers.authorization.split(" ")[1];
 
-// api routes
-app.use('/users', require('./users/users.controller'));
+  jwt.verify(token, config.secret, (error, decode) => {
+    if (error) {
+      return res.json({
+        auth: false,
+        error,
+      });
+    }
 
-// global error handler
-app.use(errorHandler);
-
-app.use(express.static('public'))
-
-// start server
-const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
-const server = app.listen(port, function () {
-    console.log('Server listening on port ' + port);
+    const user = users.find((u) => u.id === decode.sub);
+  
+    return res.json({
+      user: {
+        ...omitPassword(user),
+      },
+      auth: true,
+    });
+  });
 });
 
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
 
-module.exports = app
+  const user = users.find(
+    (u) => u.username === username && u.password === password
+  );
+
+  if (!user) throw "Username or password is incorrect";
+
+  // create a jwt token that is valid for 7 days
+  const token = jwt.sign({ sub: user.id }, config.secret, { expiresIn: "30d" });
+
+  return res.json({
+    ...omitPassword(user),
+    token,
+  });
+});
+
+// app.use(express.static('public'))
+
+// start server
+const port = process.env.NODE_ENV === "production" ? 80 : 4000;
+
+app.listen(port, function () {
+  console.log("Server listening on port " + port);
+});
+
+module.exports = app;
+
+function omitPassword(user) {
+  const { password, ...userWithoutPassword } = user;
+  return userWithoutPassword;
+}
+
+function verifyJwt(req, res, next) {
+  next();
+}
---
+++ users.js
@@ -0,0 +1,11 @@
+const users = [
+  {
+    id: "uid-1",
+    username: "username",
+    password: "password",
+    firstName: "John",
+    lastName: "Doe",
+  },
+];
+
+module.exports = users
