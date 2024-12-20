cf2bc8be74ba890f9ac1c383c3980aebd9c8f3b6
Hakimi <sulaymonilhom@gmail.com>
2024-12-20 11:04:44 +0500

    fix

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.post("/login", (req, res) => {
     (u) => u.username === username && u.password === password
   );
 
-  if (!user) throw "Username or password is incorrect";
+  if (!user) return res.status(401).send({ message: "The Username or Password is Incorrect" });
 
   // create a jwt token that is valid for 7 days
   const token = jwt.sign({ sub: user.id }, config.secret, { expiresIn: "30d" });
